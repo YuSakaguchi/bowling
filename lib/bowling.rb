abfd51e870ac015c28e384c9307d5c23413d3d8e
Yuusuke Sakaguchi <y_sakaguchi@libertyfish.co.jp>
2017-10-04 17:59:44 +0900

    Add strike score test

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -12,15 +12,25 @@ class Bowling
 
   def add_score(pins)
     @temp << pins
-    if @temp.size == 2
+    if @temp.size == 2 || strike?(@temp)
       @scores << @temp
       @temp = []
     end
   end
 
   def calc_score
-    @scores.each.with_index(1) do |score,index|
-      if spare?(score) && not_last_frame?(index)
+    @scores.each.with_index(1) do |score, index|
+      # 最終フレーム以外でのストライクなら、スコアにボーナスを含めて合計する
+      if strike?(score) && not_last_frame?(index)
+        # 次のフレームもストライクで、なおかつ最終フレーム以外なら、
+        # もう一つ次のフレームの一投目をボーナスの対象にする
+        if strike?(@scores[index]) && not_last_frame?(index + 1)
+          @total_score += 20 + @scores[index + 1].first
+        else
+          @total_score += 10 + @scores[index].inject(:+)
+        end
+        # 最終フレーム以外でのスペアなら、スコアにボーナスを含めて合計する
+      elsif spare?(score) && not_last_frame?(index)
         @total_score += calc_spare_bonus(index)
       else
         @total_score += score.inject(:+)
@@ -31,6 +41,10 @@ class Bowling
   private
     def spare?(score)
       score.inject(:+) == 10
+    end
+
+    def strike?(score)
+      score.first == 10
     end
 
     def not_last_frame?(index)
--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -57,6 +57,84 @@ describe "ボウリングのスコア計算" do
 
   end
 
+  context "ストライクを取った場合" do
+    it "ストライクボーナスが加算されること" do
+      # 1
+      @game.add_score(10)
+      # 2
+      @game.add_score(5)
+      @game.add_score(4)
+      # g
+      add_many_scores(16,0)
+      # total
+      @game.calc_score
+
+      # expect
+      # 10 + 5 + (5) + 4 + (4) = 28
+      expect(@game.total_score).to eq 28
+    end
+  end
+
+  context "ダブルを取った場合" do
+    it "それぞれのストライクボーナスが加算されること" do
+      # 1
+      @game.add_score(10)
+      # 2
+      @game.add_score(10)
+
+      # 3
+      @game.add_score(5)
+      @game.add_score(4)
+
+      #g
+      add_many_scores(14,0)
+
+      @game.calc_score
+      # 10 + 10 + (10) + 5 +(5 + 5) + 4 + (4) = 53
+      expect(@game.total_score).to eq 53
+    end
+  end
+
+  context "ターキーを取った場合" do
+    it "それぞれのストラクボーナスが加算されること" do
+      # 1
+      @game.add_score(10)
+      # 2
+      @game.add_score(10)
+      # 3
+      @game.add_score(10)
+      # 4
+      @game.add_score(5)
+      @game.add_score(4)
+
+      # G
+      add_many_scores(12,0)
+
+      # total
+      @game.calc_score
+
+      expect(@game.total_score).to eq 83
+    end
+  end
+
+  context  "最終フレームでストライクを取った場合" do
+    it "ストライクボーナスが加算されないこと" do
+      # 1
+      @game.add_score(10)
+      # 2
+      @game.add_score(5)
+      @game.add_score(4)
+      # G
+      add_many_scores(14,0)
+      # last
+      @game.add_score(10)
+      # total
+      @game.calc_score
+
+      expect(@game.total_score).to eq 38
+    end
+  end
+
   private
     def add_many_scores(count,pins)
       count.times do
